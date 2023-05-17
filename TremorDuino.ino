32d4884c59973c7877410a1112ade44ecab6e977
Eric J Bubar <ebubar@gmail.com>
2023-05-17 15:44:59 -0400

    Add files via upload
    Updated arduino code for reading accelerometer for classifying tremor and outputting a vibrating motor frequency.

---
+++ TremorDuino.ino
@@ -0,0 +1,63 @@
+/*
+  Arduino BMI270 - Simple Accelerometer
+
+  This example reads the acceleration values from the BMI270
+  sensor and continuously prints them to the Serial Monitor
+  or Serial Plotter.
+
+  The circuit:
+  - Arduino Nano 33 BLE Sense Rev2
+
+  created 10 Jul 2019
+  by Riccardo Rizzo
+
+  This example code is in the public domain.
+*/
+
+#include "Arduino_BMI270_BMM150.h"
+#include <Wire.h>
+#include "Adafruit_DRV2605.h"
+
+Adafruit_DRV2605 drv;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in G's");
+  Serial.println("X\tY\tZ");
+  drv.selectLibrary(1);
+  drv.setMode(DRV2605_MODE_INTTRIG);
+}
+
+uint8_t effect = 1;
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
+
+  drv.setWaveform(0,effect);
+  drv.setWaveform(1,0);
+  drv.go();
+  delay(500);
+  
+}
